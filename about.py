f0420e7f6ade2187634f3484e557023036b23c5f
TomasCDG <71411367+TomasCDG@users.noreply.github.com>
2024-01-24 14:13:08 -0300

    Update about.py

--- about.py
+++ about.py
@@ -4,7 +4,9 @@ https://www.linkedin.com/in/tomasclaudiodigennaro/
 tomas.digennaro@gmail.com"""
 
 intro = """Hello, I'm **Tomás Di Gennaro**\n
-Passionate *Data Scientist*, *Data Scientist Professor* and *Environmental Engineer*"""
+Passionate *Data Scientist*, *Data Scientist Professor* and *Environmental Engineer*
+Contact me : tomas.digennaro@gmail.com / tomas.digennaro@globant.com
+"""
 
 core =  """On this Streamlit page, you'll find information about some of my educational certificates.
 \nMost of my certificates are data-related: Data Science, LLMs, ML Engineering.
