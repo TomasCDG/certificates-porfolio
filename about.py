7cb98cc78666afa3ac3906ee66368ce04d237360
TomasCDG <71411367+TomasCDG@users.noreply.github.com>
2024-01-24 14:09:40 -0300

    Update about.py

--- about.py
+++ about.py
@@ -6,7 +6,7 @@ tomas.digennaro@gmail.com"""
 intro = """Hello, I'm **Tomás Di Gennaro**\n
 Passionate *Data Scientist* and *Environmental Engineer*"""
 
-core =  """On this humble Streamlit page, you'll find information about some of my educational certificates.
+core =  """On this Streamlit page, you'll find information about some of my educational certificates.
 \nMost of my certificates are data-related: Data Science, LLMs, ML Engineering.
 \nBut that's not all! I've taken the liberty of attaching my Environmental Engineering degree.
 \nI haven't yet scanned the rest of my engineering certifications, including university diplomas and Udemy certificates, but I hope you don't mind, for now. 
